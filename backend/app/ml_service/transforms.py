16a5eea75949b71d9580c19ac96de4b736d115b8
Karl <khjohansen97@hotmail.com>
2025-10-31 18:07:15 +0100

    update transforms with same values as trained model

--- backend/app/ml_service/transforms.py
+++ backend/app/ml_service/transforms.py
@@ -2,15 +2,13 @@ from torchvision import transforms
 from PIL import Image
 import torch
 
-# TODO: Må matche med vår pipeline? 
 transform_pipeline = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                         std=[0.229, 0.224, 0.225])
+    transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                     std=[0.5, 0.5, 0.5])
 ])
 
 def preprocess_image(image: Image.Image) -> torch.Tensor:
-    # TODO: Apply transforms and add batch dimension
     tensor = transform_pipeline(image).unsqueeze(0)
     return tensor
